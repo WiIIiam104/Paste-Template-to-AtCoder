7e8e10605509e4ab30c49e1aee2feef79c0877a8
WiIIiam104 <96721354+WiIIiam104@users.noreply.github.com>
2022-03-02 21:28:40 +0900

    Create UserScript.js

---
+++ UserScript.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         paste template
+// @namespace    http://atcoder.jp/
+// @version      0.2.4
+// @description  paste your template to atcoder
+// @author       wiiiiam104
+// @match        https://atcoder.jp/contests/*/tasks/*
+// @match        https://atcoder.jp/contests/*/custom_test*
+// @grant        unsafeWindow
+// ==/UserScript==
+
+
+(function script() {
+  // yourCode(string) will be saved by running below (recommend using String.raw)
+  // localStorage.template=(yourCode);
+
+  const f=(async () => {
+    const data = localStorage.template;
+    if (data===undefined)return alert("no saved code found");
+    if ($('.btn-toggle-editor').click().click(),!document.querySelector('span.cm-variable') || confirm("Your current code will be replaced. Are you sure?")) {
+      $('.plain-textarea').val(data);
+      $('.editor').data('editor').doc.setValue(data);
+    }
+  });
+
+  $("<a>", {
+    class:"btn btn-danger btn-sm",
+    style:"margin:10px 0;display:block;width:128.95px;"
+  })
+  .html("Paste Your Template")
+  .click(f)
+  .appendTo(".col-sm-3.editor-buttons");
+
+})();
