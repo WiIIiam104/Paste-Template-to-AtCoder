b871d87f0f6fd1080f603bd74a016ddff4c00616
WiIIiam104 <96721354+WiIIiam104@users.noreply.github.com>
2022-03-15 15:30:04 +0900

    Update UserScript.js

--- UserScript.js
+++ UserScript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         paste template
-// @namespace    http://github.com/wiiiiam104
-// @version      0.3.3
+// @namespace    https://github.com/wiiiiam104
+// @version      0.3.4
 // @description  paste your template to atcoder
 // @author       wiiiiam104
 // @match        https://atcoder.jp/contests/*/tasks/*
